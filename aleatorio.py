f5546c5f5f2e8a6577eaca4bbe20733e65849412
Fabio Luiz Marques da Silva <flms@palmares.ifpe.edu.br>
2026-09-02 10:21:21 -0300

    atualização

--- aleatorio.py
+++ aleatorio.py
@@ -1,9 +1,20 @@
 import random
 mega=[]
+respostas=[]
+acertos=0
 print('ola, seja bem vindo a megacena da virada')
-for i in range (7):
-    dig=int(input('digite um numero de 1 a 20: '))
-    num=random.randint(1, 20)
+for i in range(7):
+    dig=int(input('digite um numero de 1 a 60: '))
+    if dig<60:
+        respostas.append(dig)
+
+    elif dig>60:
+        print('numero não aceito')
+
+    num=random.randint(1, 60)
     mega.append(num)
-megacena=mega
-print (megacena)
+    for m,r in zip(mega, respostas):
+        if m == r:
+            acertos+=1
+            print(acertos)
+
